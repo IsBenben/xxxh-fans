89438fb0afc2e0b09b32c56ec700b7f07e8bea6a
IsBenben <542863140@qq.com>
2026-03-19 21:35:55 +0800

    fix change folder

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-s = fetch_fans_count(uid)
-            # if current_fans is None:
-            #     print(f"Failed to fetch fans count of {uid}.")
-            #     continue
-            
-            # now = datetime.now(ZoneInfo('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M")
-            # history.setdefault(uid, []).append({"date": now, "count": current_fans})
